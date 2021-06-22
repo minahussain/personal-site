84d49f898155713e9e66b43479fc0f08fdd23c1a
saminahussain <saminahu@gmail.com>
2021-06-22 15:18:16 -0700

    formatted css for projects, needs graphql query fixed

---
+++ data/projects/spotifollow/index.md
@@ -0,0 +1,14 @@
+---
+date: '2019-06-13'
+title: 'SpotiFollow'
+cover: './demo.png'
+github: 'https://github.com/minahussain/spotifollow/'
+external: ''
+tech:
+  - Python
+  - Django
+  - AWS
+showInProjects: true
+---
+
+A web app that allows Spotify users to "follow" another user's playlist by copying their playlist so you can add or remove songs at your leisure. In the web app, users can update the playlist to match any changes made to the original playlist, while maintaining any of your removals or additions. Spotify recently added a "hide" feature in playlists to remove songs, but still with no functionality to add songs.
--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -1,42 +1,77 @@
 import React from 'react';
 import Portrait from '../../images/portrait1.png'
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 
 const Projects = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      markdownRemark(
+        filter: { fileAbsolutePath: { regex: "/projects/" } }
+      ) {
+        html
+        frontmatter {
+            date
+            title
+            cover
+            github
+            external
+            tech
+            showInProjects
+        }
+      }
+    }
+  `);
+    const projects = data.projects.edges.filter(({ node }) => node);
 
-  return (
-    <section id="projects" className="jumbotron">
-        <h2>Projects</h2>
-        <div className="row">
-            <div className="column">
-                <div className="card">
-                    <img src={Portrait} alt="Avatar" style={{"width": "100%"}} />
-                    <div className="container">
-                        <h4><b>Project Name</b></h4>
-                        <p>project description</p>
+    return (
+        <section id="projects" className="jumbotron">
+            <h2>Projects</h2>
+
+            { projects && projects.map(({ node }, i) => {
+                const { frontmatter, html } = node;
+                const { external, title, tech, github, cover } = frontmatter;
+                const image = getImage(cover);
+
+                return (
+                    <div className="project">
+                        <div className="project-img">
+                            <img src={Portrait} alt="Avatar" style={{"width": "100%"}} />
+                            //<GatsbyImage image={image} alt={title} className="img" />
+                        </div>
+                        <div className="card">
+                            <div className="project-title">
+                                {title}
+                            </div>
+                            <div className="project-description"
+                                 dangerouslySetInnerHTML={{ __html: html }}>
+                            </div>
+                            {tech.length && (
+                            <ul className="project-tech">
+                                {tech.map((tech, i) => (
+                                <li key={i}>{tech}</li>
+                                ))}
+                            </ul>
+                            )}
+                            <div className="project-links">
+                                {github && (
+                                    <a href={github} aria-label="GitHub Link">
+                                    <FaGithub />
+                                    </a>
+                                )}
+                                {external && (
+                                    <a href={external} aria-label="External Link" className="external">
+                                    <FaExternalLinkAlt />
+                                    </a>
+                                )}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="column">
-                <div className="card">
-                    <img src={Portrait} alt="Avatar" style={{"width": "100%"}} />
-                    <div className="container">
-                        <h4><b>Project Name</b></h4>
-                        <p>project description</p>
-                    </div>
-                </div>
-            </div>
-            <div className="column">
-                <div className="card">
-                    <img src={Portrait} alt="Avatar" style={{"width": "100%"}} />
-                    <div className="container">
-                        <h4><b>Project Name</b></h4>
-                        <p>project description</p>
-                    </div>
-                </div>
-            </div> 
-        </div>
-    </section>
-  );
+                );
+            })}
+        </section>
+    );
 };
 
 export default Projects;
