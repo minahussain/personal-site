56c48d07ef69aa67a33527669142450a3b904249
saminahussain <saminahu@gmail.com>
2021-08-19 22:36:27 -0700

    add alias for config using node api

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+// https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@config': path.resolve(__dirname, 'src/config'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+      },
+    },
+  });
+};
