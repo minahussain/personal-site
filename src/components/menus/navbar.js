0ca7c6c4e4040f087309ba0b199e55133db57fa9
saminahussain <saminahu@gmail.com>
2021-08-31 16:32:31 -0700

    update nav menu for mobile

--- src/components/menus/navbar.js
+++ src/components/menus/navbar.js
@@ -1,46 +1,75 @@
 import React, { useState, useEffect } from "react";
+import { Helmet } from 'react-helmet';
 import { Link } from "react-scroll";
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
 import { navbarLinks } from '@config';
 
 const Navbar = () => {
     let [show, setShow] = useState(false);
+    const [navbarOpen, setNavbarOpen] = useState(false)
 
     useEffect(() => {
         const timeout = setTimeout(() => {setShow(true)}, 100);
         return () => {clearTimeout(timeout)}
     }, []);
 
+    const handleToggle = () => {
+        setNavbarOpen(prev => !prev)
+    }
+
+    const closeMenu = () => {
+        setNavbarOpen(false)
+    }
+
     return (
-        <nav className="nav" id="navbar">
-            <div className="nav-content">
-                <ul className="nav-items">
+        <>
+            <Helmet>
+                <body className={navbarOpen ? 'noscroll' : ''} />
+            </Helmet>
+            <nav className={`nav ${navbarOpen ? " showMenu" : ""}`} id="navbar">
+                <div className={`nav-menu ${navbarOpen ? " showMenu" : ""}`}>
+                    <button className="hamburger-button" onClick={handleToggle}>
+                        {navbarOpen ? (
+                            <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+                        ) : (
+                            <FiMenu style={{ color: "#fff", width: "40px", height: "40px" }} />
+                        )}
+                    </button>
+                    <ul className={`nav-content ${navbarOpen ? " showMenu" : ""}`}>
+                        <TransitionGroup component={null}>
+                          { show && 
+                            navbarLinks.map(({ name, url }, i) => (
+                              <CSSTransition key={i} classNames="fade" timeout={100}>
+                                <li key={i} className="nav-item" style={{ transitionDelay: `${i * 8}00ms` }}>
+                                    <Link 
+                                        activeClass="active" 
+                                        to={url} 
+                                        /*spy={true}*/
+                                        smooth={true}
+                                        onClick={() => closeMenu()}
+                                    >
+                                        {name}
+                                    </Link>
+                                </li>
+                              </CSSTransition>
+                            ))}
+                        </TransitionGroup>
+                    </ul>
                     <TransitionGroup component={null}>
-                      { show && 
-                        navbarLinks.map(({ name, url }, i) => (
-                          <CSSTransition key={i} classNames="fade" timeout={100}>
-                            <li key={i} className="nav-item" style={{ transitionDelay: `${i * 8}00ms` }}>
-                                <Link activeClass="active" to={url} /*spy={true}*/smooth={true}>
-                                    {name}
-                                </Link>
-                            </li>
-                          </CSSTransition>
-                        ))}
-                    </TransitionGroup>
-                    <TransitionGroup component={null}>
-                        {show && (
-                          <CSSTransition classNames="fade" timeout={100}>
-                            <li key="resume" 
-                                className="nav-item" 
+                        { show && (
+                            <CSSTransition classNames="fade" timeout={1000}>
+                              <div key="resume" className="nav-button nav-item" 
                                 style={{ transitionDelay: `${navbarLinks.length * 700}ms` }}>
-                                <button className="btn-transparent"><a href="#/hero">Resume</a></button>
-                            </li>
-                          </CSSTransition>
+                                    <button><a href="#/hero">Resume</a></button>
+                                </div>
+                            </CSSTransition>
                         )}
                     </TransitionGroup>
-                </ul>
-            </div>
-        </nav>
+                </div>
+            </nav>
+        </>
     );
 };
 
